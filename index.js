db83fb0b84031719005ca8a6df795667d500cc96
Mikołaj <mikolajhoscilo@gmail.com>
2020-10-17 02:56:45 +0200

    Remove routes

--- index.js
+++ index.js
@@ -9,22 +9,21 @@ app.use(helet());
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
-
 app.use(express.static('./public'));
 
-app.get('/url/:id', (req, res) => {
-    // TODO: get a short url by id
-});
+// app.get('/url/:id', (req, res) => {
+//     // TODO: get a short url by id
+// });
 
-app.get('/:id', (req, res) => {
-    // TODO: redirect to the URL
-});
+// app.get('/:id', (req, res) => {
+//     // TODO: redirect to the URL
+// });
 
-app.post('/url', (req, res) => {
-    // TODO: create a short URL
-});
+// app.post('/url', (req, res) => {
+//     // TODO: create a short URL
+// });
 
-const port = process.env.port || 7777;
+const port = process.env.PORT || 7777;
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
 })
